b509463b124eadeb63152aba570758ec790c3299
mevett <mevett@emich.edu>
2023-10-24 12:01:38 -0400

    Generating a push dialog box for rubric.

--- src/eclipseSoloProj/HelloWorld.java
+++ src/eclipseSoloProj/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("This program rocks!");
-		System.out.println("Another very good print statement is very good.");
+		System.out.println("Another very good print statement is very good."); 
 	}
 
 }
